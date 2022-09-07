9f3bd7b158b3026980389c5d1ce5a6c66b06aed3
Godswill Udoh <godswilludoh@gmail.com>
2022-09-07 06:12:29 +0100

    initial repo

---
+++ sumNumbers.js
@@ -0,0 +1,17 @@
+let numbers = [1, 2, 3, 4, 5];
+let sum = 0;
+for (let i = 0; i < numbers.length; i++) {
+	sum += numbers[i];
+}
+console.log(sum);
+
+//putting inside a function
+
+function addNumber(array) {
+	let sum = 0;
+	for (let i = 0; i < array.length; i++) {
+		sum += array[i];
+	}
+	return sum;
+}
+console.log(addNumber([1, 2, 3, 4, 5]));
